f00bda664e175612bc7d38c6a9a3b67fb9cea9ea
Huseyn Tapdigli <74926067+huseynt@users.noreply.github.com>
2022-07-05 00:40:29 +0400

    Add files via upload

---
+++ car_marketing_site/script/script.js
@@ -0,0 +1,20 @@
+window.addEventListener('scroll', function () {
+    var scroll = document.querySelector('.main');
+    scroll.classList.toggle("active", window.scrollY > 300)
+});
+
+
+window.addEventListener('scroll', function () {
+    var scroll2 = document.querySelector('.main_2');
+    scroll2.classList.toggle("active", window.scrollY>300 & window.scrollY<1400);
+});
+
+window.addEventListener('scroll', function () {
+    var scroll2 = document.querySelector('.main_3');
+    scroll2.classList.toggle("active", window.scrollY>1400 & window.scrollY<2100);
+});
+
+// window.addEventListener('scroll', function (e) {
+//     console.log(window.scrollY)
+// });
+
